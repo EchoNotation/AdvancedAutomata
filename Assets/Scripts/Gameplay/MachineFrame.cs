d3224e61639ecd487c2f912ad57236b9d8b09bd1
Thomas <thetardis1968@gmail.com>
2019-12-31 14:37:13 -0600

    Test scene has a movable unit

--- Assets/Scripts/Gameplay/MachineFrame.cs
+++ Assets/Scripts/Gameplay/MachineFrame.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class MachineFrame : MonoBehaviour
 {
+    public enum FrameType {
+        LIGHT,
+        MEDIUM,
+        HEAVY,
+        FLYING,
+    }
+
+    public enum HardpointType {
+        WEAPON,
+        ARMOR,
+        UTILITY,
+    }
+
+    private FrameType type;
+    //0 : Weapon, 1 : Armor, 2 : Utility
+    private int[] numOfHardpoints;
+    private int[] numOfAvailableHardpoints;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +32,32 @@ public class MachineFrame : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void configure(FrameType fType)
+    {
+        type = fType;
+        numOfHardpoints = new int[3];
+        numOfAvailableHardpoints = new int[3];
+
+        switch(fType)
+        {
+            case FrameType.LIGHT:
+                break;
+            case FrameType.MEDIUM:
+                break;
+            case FrameType.HEAVY:
+                break;
+            case FrameType.FLYING:
+                break;
+            default:
+                Debug.Log("Invalid FrameType in MachineFrame-- Did you forgot to add a new case? FrameType: " + fType.ToString());
+                break;
+        }
+    }
+
+    void connectHardpoint(Accessory accessory)
+    {
+
     }
 }
---
+++ Assets/Scripts/Gameplay/SimpleMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xVelocity = 0, yVelocity = 0;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            yVelocity += 0.1f;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            xVelocity -= 0.1f;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            xVelocity += 0.1f;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            yVelocity -= 0.1f;
+        }
+
+        this.transform.SetPositionAndRotation(new Vector3(transform.position.x + xVelocity, transform.position.y + yVelocity, 0), Quaternion.identity);
+    }
+}
